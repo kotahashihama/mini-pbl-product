e1edb2c542339d010c858d9e1886b9121ba84a2b
daiphone0228 <daiphone0228@gmail.com>
2022-12-30 11:36:08 +0900

    comments.contributorをnot nullに変更

--- project/db/migrate/20221226034219_create_comments.rb
+++ project/db/migrate/20221226034219_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.string      :contributor, index: true
+      t.string      :contributor, null: false, index: true
       t.text        :comment, null: false
       t.integer     :post_id, null: false, index: true
       t.integer     :user_id, index: true
