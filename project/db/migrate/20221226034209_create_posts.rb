89418e363a03c0b18ff3e01c1cd88b7fe65fc233
daiphone0228 <daiphone0228@gmail.com>
2022-12-30 11:26:23 +0900

    posts.detailとcomments.commentのデータ型をstringからtext型に変更

--- project/db/migrate/20221226034209_create_posts.rb
+++ project/db/migrate/20221226034209_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string      :title, null: false, index: true
-      t.string      :detail, null: false
+      t.text        :detail, null: false
       t.datetime    :deleted_at
 
       t.timestamps
--- project/db/migrate/20221226034219_create_comments.rb
+++ project/db/migrate/20221226034219_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.string      :contributor, index: true
-      t.string      :comment, null: false
+      t.text        :comment, null: false
       t.integer     :post_id, null: false, index: true
       t.integer     :user_id, index: true
       t.datetime    :deleted_at
