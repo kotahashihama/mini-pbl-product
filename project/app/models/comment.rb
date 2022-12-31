a0f705e5df4ae0b3e8e39235a156a337e65e9c4b
Kota Hashihama <kotahashihama@gmail.com>
2022-12-31 19:54:40 +0900

    feat(/posts/*): コメントの CR ロジックを追加

--- project/app/models/comment.rb
+++ project/app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  validates :contributor, length: { maximum: 50 }
+  validates :comment, presence: true, length: { maximum: 2000 }
+  validates :post_id, presence: true
 end
