f6b93e47c0a4e0daa24cf09c7b460e24498a9657
daiphone0228 <daiphone0228@gmail.com>
2023-01-12 02:39:29 +0900

    スレッド一覧画面のスレッド一覧取得、スレッド作成機能を実装

--- project/app/controllers/posts_controller.rb
+++ project/app/controllers/posts_controller.rb
@@ -1,6 +1,29 @@
 class PostsController < ApplicationController
+
+  # def new
+
+  # end
+
+  def index
+    # スレッド一覧取得メソッド
+    Rails.logger.debug "index method called"
+    @posts = Post.all
+    @post = Post.new
+  end
+
   def show
     @comments = Comment.all
     @comment = Comment.new
+  end
+
+  def create
+    Rails.logger.debug "create method called"
+    @post = Post.create(post_params)
+
+    redirect_to controller: :posts, action: :index
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :detail)
   end
 end
--- project/app/models/post.rb
+++ project/app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+    validates :title, length: { maximum: 100 }
+    validates :detail, presence: true, length: { maximum: 1000 }
 end
