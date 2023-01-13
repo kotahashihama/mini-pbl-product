9471d503df9f438b112a1630d6fa97d93db9fbb4
Kota Hashihama <kotahashihama@gmail.com>
2023-01-13 13:27:45 +0900

    feat(users): 新規登録ができるところまでを確立

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :posts
   resources :comments
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
   get "/sign-up", to: "users#new"
   get "/login", to: "sessions#new"
 end
