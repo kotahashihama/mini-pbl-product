18a2bd7f73d835d42fd8ff2e2de44fa8e2f8a4f2
Kota Hashihama <kotahashihama@gmail.com>
2023-01-13 09:58:41 +0900

    feat(devise): devise 関連のファイルを追加

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   root "posts#index"
   resources :posts
   resources :comments
 
+  devise_for :users
   get "/sign-up", to: "users#new"
   get "/login", to: "sessions#new"
 end
