e3ac53fabacbf0b82a87a724227acc1da920f57b
ohno hiroto <@aiit.ac.jp>
2023-01-17 19:10:31 +0900

    modify html and controller

--- project/app/controllers/posts_controller.rb
+++ project/app/controllers/posts_controller.rb
@@ -3,5 +3,17 @@ class PostsController < ApplicationController
     @comments = Comment.all
     @comment = Comment.new
   end
+
+  # 編集中
+  def index
+    @posts = Post.all
+    @post = Post.new
+
+    @comments = Comment.all
+    @comment = Comment.new
+    @comments_first = Comment.first
+    @comments_last = Comment.last(3)
+    
+  end
 end
 
