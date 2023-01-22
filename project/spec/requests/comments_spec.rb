adf33c200010386a43cf0986908cf27380e6657c
Kota Hashihama <kotahashihama@gmail.com>
2023-01-22 20:45:35 +0900

    fix(controller): rubocop エラーを解消

--- project/spec/requests/comments_spec.rb
+++ project/spec/requests/comments_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "Comments" do
     example 'コメントを作成できる' do
       post_params = { comment: { contributor: 'ななしくん', comment: 'はろー', post_id: 1 } }
       post comments_path, params: post_params
+
       expect(response).to have_http_status(:found)
     end
   end
