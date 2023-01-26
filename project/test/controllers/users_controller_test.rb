ca25dd006398bd836dc1ebcd5935e5e1cf931030
Kota Hashihama <kotahashihama@gmail.com>
2023-01-27 00:21:28 +0900

    test(users_controller.rb): 必要のないテストコードを削除

--- project/test/controllers/users_controller_test.rb
+++ project/test/controllers/users_controller_test.rb
@@ -1,8 +1,7 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get users_show_url
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
