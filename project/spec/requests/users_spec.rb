e21e78bd890cf701a5d31c99080b5ec7e287187d
Kota Hashihama <kotahashihama@gmail.com>
2023-01-27 00:26:07 +0900

    fix(users_spec.rb): rubocop エラーを解消

--- project/spec/requests/users_spec.rb
+++ project/spec/requests/users_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Users" do
-  # describe 'should get show' do
-  #   context 'sign_in' do
-  #     example 'ログイン' do
-  #       （ここにログインする処理を書く）
-  #       expect(assert_response).to eq(:success)
-  #     end
-  #   end
+# RSpec.describe "Users" do
+#   describe 'should get show' do
+#     context 'sign_in' do
+#       example 'ログイン' do
+#         （ここにログインする処理を書く）
+#         expect(assert_response).to eq(:success)
+#       end
+#     end
 
-  #   context 'sign_out' do
-  #     example 'ログアウト' do
-  #       （ここにログアウトするリクエストを書く）
-  #       expect(assert_response).to eq(:success)
-  #     end
-  #   end
-  # end
-end
+#     context 'sign_out' do
+#       example 'ログアウト' do
+#         （ここにログアウトするリクエストを書く）
+#         expect(assert_response).to eq(:success)
+#       end
+#     end
+#   end
+# end
