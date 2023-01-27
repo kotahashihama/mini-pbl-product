5f452be08589b4d06a730543bcb62630693eb2dd
Kota Hashihama <kotahashihama@gmail.com>
2023-01-27 10:11:37 +0900

    fix: RSpec エラーを解消

--- project/app/controllers/users_controller.rb
+++ project/app/controllers/users_controller.rb
@@ -1,3 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:mypage]
+
+  def mypage
+    @comments = Comment.where(user_id: current_user.id).order(created_at: :desc)
+  end
 end
