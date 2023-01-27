15238bda00e1800e1b2ec9069e448cb73b9d04f0
Kota Hashihama <kotahashihama@gmail.com>
2023-01-27 09:42:57 +0900

    feat(mypage): コメント履歴を実装

--- project/app/models/comment.rb
+++ project/app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  belongs_to :user
   validates :contributor, length: { maximum: 50 }
   validates :comment, presence: true, length: { maximum: 2000 }
 end
